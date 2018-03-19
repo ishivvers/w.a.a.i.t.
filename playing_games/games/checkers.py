cea7374bca71e554053e5c7541a18a4379d98372
Isaac Shivvers <ishivvers@airware.com>
2018-03-19 16:35:03 -0700

    Status: good working state
    Game seems to be in good shape, with rules appropriately codified,
    though further work will be required to mesh this with the MCTS
    appropriately.

--- playing_games/games/checkers.py
+++ playing_games/games/checkers.py
@@ -1,21 +1,32 @@
 import numpy as np
+from random import choice
 
 class board():
 
-    def __init__(self):
+    def __init__(self, board=None):
+        """
+        Args:
+            board (8x8 np array): the state of the board.
+                if not given, initializes a new game board.
+        """
         self.player = 1  # 1 (red) or 2 (black)
+        self.piece_in_play = None
+        self.plays_without_capture = 0
         size = 8
-        self.board = np.zeros((size, size), dtype=np.int8)
         self.cols, self.rows = np.meshgrid(range(size), range(size))
-        # mask out the unavailable spots, and put pieces down
-        for i in range(self.board.shape[0]):
-            for j in range(self.board.shape[1]):
-                if (i + j) % 2:
-                    self.board[i, j] = -1
-                elif i <= 2:
-                    self.board[i, j] = 1
-                elif i > 4:
-                    self.board[i, j] = 2
+        if board == None:
+            self.board = np.zeros((size, size), dtype=np.int8)        
+            # mask out the unavailable spots, and put pieces down
+            for i in range(self.board.shape[0]):
+                for j in range(self.board.shape[1]):
+                    if (i + j) % 2:
+                        self.board[i, j] = -1
+                    elif i <= 2:
+                        self.board[i, j] = 1
+                    elif i > 4:
+                        self.board[i, j] = 2
+        else:
+            self.board = board
 
     def _p1_pieces(self):
         return (self.board == 1) | (self.board == 11)
@@ -23,12 +34,20 @@ class board():
         return (self.board == 2) | (self.board == 22)
 
     def winner(self):
+        """
+        If a player has won, return their player number.
+        If we have a tie, return 0.
+        Otherwise, return None.
+        """
         p1mask = self._p1_pieces()
         p2mask = self._p2_pieces()
         if not np.sum(p1mask):
             return 2
         elif not np.sum(p2mask):
             return 1
+        elif self.plays_without_capture > 50:
+            # call this a tie
+            return 0
         else:
             return None
 
@@ -36,13 +55,14 @@ class board():
         """
         Return the current state of the game
         """
-        return (self.board, self.winner(), self.player)
+        return (self.board, self.player, self.piece_in_play, self.winner())
 
     def legal_moves(self):
         """
         Create a list of the possible moves, given the current game state.
 
-        Returns: list of list, each containing three entries (start, end, jumped)
+        Returns: list of list, each containing three entries as tuples of integers
+            (start, end, jumped)
         """
         p1mask = self._p1_pieces()
         p2mask = self._p2_pieces()
@@ -57,65 +77,92 @@ class board():
         others = set(zip(*(self.rows[omask], self.cols[omask])))
         occupied = pieces.union(others)
 
+        def _row_moves(i, j):
+            if self.board[i, j] > 10:
+                # is kinged, can move either way
+                return [-1, 1]
+            elif self.player == 1:
+                # can only move down
+                return [1]
+            else:
+                # can only move up
+                return [-1]
+
         def _possible_jumps(i, j, row_moves):
             possibilities = []
             for r in row_moves:
                 # jumping to the right
                 if (i + 2*r < 8 and i + 2*r >= 0 and j < 6) and\
                    (i + r, j + 1) in others and (i + 2*r, j + 2) not in occupied:
-                    possibilities += [[(i, j), (i + 2*r, j + 2), [(i + r, j + 1)]]]
+                    possibilities += [[(i, j), (i + 2*r, j + 2), (i + r, j + 1)]]
                 # jumping to the left
                 elif (i + 2*r < 8 and i + 2*r >= 0 and j > 1) and\
                      (i + r, j - 1) in others and (i + 2*r, j - 2) not in occupied:
-                    possibilities += [[(i, j), (i + 2*r, j - 2), [(i + r, j - 1)]]]
+                    possibilities += [[(i, j), (i + 2*r, j - 2), (i + r, j - 1)]]
             return possibilities
 
-        possibilities = []
-        for p in pieces:
-            i, j = p
-            if self.board[i, j] > 10:
-                # is kinged, can move either way
-                row_moves = [-1, 1]
-            elif self.player == 1:
-                # can only move down
-                row_moves = [1]
-            else:
-                # can only move up
-                row_moves = [-1]
-            # find all the normal moves
-            possibilities += [(p, (i + r, j + c), None) for r in row_moves for c in [-1, 1] 
-                              if min(i + r, j + c) >= 0 and max(i + r, j + c) < 8 and (i + r, j + c) not in occupied]
-            # find all the jumps: does not yet include multiple jumps in a row
-            jumps = _possible_jumps(i, j, row_moves)
-            possibilities += jumps
+        def _possible_normal_moves(i, j, row_moves):
+            return [((i, j), (i + r, j + c), None)
+                     for r in row_moves
+                     for c in [-1, 1]
+                     if min(i + r, j + c) >= 0
+                     and max(i + r, j + c) < 8
+                     and (i + r, j + c) not in occupied]
+
+        if self.piece_in_play:
+            i, j = self.piece_in_play
+            possibilities = _possible_jumps(i, j, _row_moves(i, j))
+        else:
+            possibilities = []
+            # find all possible jump moves first
+            for i, j in pieces:
+                possibilities += _possible_jumps(i, j, _row_moves(i, j))
+            # if no jump moves exist, and we're not in the middle of a jump, search for normal moves
+            if not possibilities:
+                for i, j in pieces:
+                    possibilities += _possible_normal_moves(i, j, _row_moves(i, j))
+
         return possibilities
 
-    def move(self, start, end, jumped=None):
+    def _switch_player(self):
+        if self.player == 1:
+            self.player = 2
+        else:
+            self.player = 1
+
+    def single_move(self, start, end, jumped=None):
         """
-        Complete a move.  Does not check for legality of move.
+        Complete a single move.
+        Does not check for legality of move.
+        Does perform some book-keeping about which player's turn it is and
+        (if we're in the middle of a jump move) which piece is currently in play.
 
         Args:
-            start (tuple of ints): column, row of piece's starting point
-            end (tuple of ints): column, row of piece's ending point
-            jumped list of (tuples of ints): if given, will remove these pieces
+            start (tuple of ints): row, column of piece's starting point
+            end (tuple of ints): same, for piece's ending point
+            jumped (tuple of ints): same; if given, will remove this piece
         """
         board = self.board.copy()
         board[end[0], end[1]] = self.board[start[0], start[1]]
         board[start[0], start[1]] = 0
-        if jumped is not None:
-            # got 'em
-            for j in jumped:
-                board[j[0], j[1]] = 0
         if self.player == 1:
             if end[0] == 7:
                 # king me
                 board[end[0], end[1]] = 11
-            self.player = 2
         else:
             if end[0] == 0:
                 # king me
                 board[end[0], end[1]] = 22
-            self.player = 1
+        if jumped is not None:
+            # got 'em
+            board[jumped[0], jumped[1]] = 0
+            self.piece_in_play = end
+            self.plays_without_capture = 0
+        else:
+            # end of move: no pieces in play, and switch the user
+            self.piece_in_play = None
+            self._switch_player()
+            self.plays_without_capture += 1
         self.board = board
 
     def display(self):
@@ -138,3 +185,45 @@ class board():
             picture += '{}\n'.format(chars[-1])
         picture += chars['reset']
         print(picture)
+
+    def next(self):
+        """
+        Take the next move, randomly chosen.
+        Handles some book-keeping about game state.
+        """
+        win = None
+        _moves = self.legal_moves()
+        if self.piece_in_play and not _moves:
+            # the extended jump move is over
+            self._switch_player()
+            self.piece_in_play = None
+            _moves = self.legal_moves()
+        if not _moves:
+            # player has no available moves; other player wins
+            self._switch_player()
+            win = self.player
+        else:
+            # randomly play a move
+            self.single_move(*choice(_moves))
+            self.display()
+            win = self.winner()
+        if win is not None:
+            print('Game is over!')
+            if win == 1:
+                winner = 'red'
+            elif win == 2:
+                winner = 'black'
+            elif win == 0:
+                winner = 'tie'
+            print('Winner:', winner)
+            return win
+
+    def run_random(self):
+        """
+        Run a whole game out, with each player randomly choosing amongst legal moves.
+        """
+        while True:
+            res = self.next()
+            if res is not None:
+                break
+
